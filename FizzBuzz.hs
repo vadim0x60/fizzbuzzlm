dda624a88f1aa172a1b1ffca670778dc7cb54c48
Vadim Liventsev <dev@vadim.me>
2024-01-19 13:29:12 +0100

    Comments are for losers

--- FizzBuzz.hs
+++ FizzBuzz.hs
@@ -6,8 +6,6 @@ fizzbuzz n =
     show n <|> [fizz| n `mod` 3 == 0] ++ 
                [buzz| n `mod` 5 == 0]
 
--- A simple default choice operator. 
--- Defaults if both fizz and buzz fail, concats if any succeed.
 infixr 0 <|>
 d <|> [] = d
 _ <|> x = concat x
