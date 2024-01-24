017d0a5911d6f748641b99abd99ae07b0849edb2
Vadim Liventsev <dev@vadim.me>
2024-01-24 14:36:22 +0100

    Perfect Python FizzBuzz

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,5 +1,5 @@
 def fizzbuzz(num):
-    return ("Fizz" if num % 3 == 0 else "") + ("Buzz" if num % 5 == 0 else "") or str(num)
+    return "Fizz" * (num % 3 == 0) + "Buzz" * (num % 5 == 0) or str(num)
 
 if __name__ == '__main__':
     for i in range(1, 101):
